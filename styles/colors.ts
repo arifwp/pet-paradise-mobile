3a2af0c7a94e190823d7fdcf9807ca53d93d6005
Arif Wahyu <arifwahyu696@gmail.com>
2025-11-26 00:05:47 +0700

    base styling

--- styles/colors.ts
+++ styles/colors.ts
@@ -1,7 +1,8 @@
 export const colors = {
-  primaryColor: "#047857",
+  primary: "#047857",
   primaryBlack: "#0a0a0a",
   white: "#FFFFFF",
-  bgNeutral: "#f5f5f5",
+  bgNeutral200: "#f5f5f5",
+  bgNeutral300: "#e5e5e5",
   secondaryText: "#737373",
 };
--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -7,7 +7,7 @@ export const globalStyle = StyleSheet.create({
     flexDirection: "column",
     padding: 24,
     gap: 24,
-    backgroundColor: colors.bgNeutral,
+    backgroundColor: colors.white,
   },
   containerRow: {
     flexDirection: "row",
@@ -15,9 +15,17 @@ export const globalStyle = StyleSheet.create({
     alignItems: "center",
   },
   logoSm: {
-    width: 64,
-    height: 64,
+    width: 72,
+    height: 72,
     resizeMode: "contain",
     borderRadius: 6,
+  },
+  primaryLink: { fontWeight: 600, color: colors.primary },
+  backButton: {
+    padding: 4,
+    backgroundColor: colors.bgNeutral300,
+    borderRadius: 100,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
